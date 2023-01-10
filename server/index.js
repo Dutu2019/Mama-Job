856105e3d3df505e7765f0527bba1adbd2501826
Dutu2019 <erictanase2007@gmail.com>
2023-01-10 18:12:02 -0500

    Added hours.json database

--- server/index.js
+++ server/index.js
@@ -13,8 +13,15 @@ app.use((req, res, next) => {
 });
 
 app.post("/addHour", (req, res) => {
-  const startTime = req.body;
-  console.log(startTime);
+  const startTime = req.body.startTime;
+  const endTime = req.body.endTime;
+  const breakTime = req.body.breakTime;
+  const data = { startTime: startTime, endTime: endTime, breakTime: breakTime };
+
+  fs.writeFile("hours.json", JSON.stringify(data), (err) => {
+    if (err) console.log(err);
+    else console.log(fs.readFileSync("hours.json", "utf8"));
+  });
 });
 
 app.listen(3001, "0.0.0.0", () => {
